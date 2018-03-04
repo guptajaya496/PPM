3fb6f05d21d948586e6a29b203fa3e40700100e6
JG <mail2me17@gmail.com>
2018-03-03 22:32:08 -0800

    app changes

--- containers/header.jsx
+++ containers/header.jsx
@@ -31,7 +31,7 @@ class Header extends Component{
             <div>
                 <nav className=" navbar-expand-lg bg-color navbar-fixed-top nav " role="navigation">
                     <a className="navbar-brand" href='/'>
-                        <img src="../images/BooksRFriendsLogo.jpg" alt="logo" style={{width:200+'px'}}></img>
+                        <img src="../images/PPM.jpg" alt="logo" style={{width:200+'px'}}></img>
                     </a>
                     <div id="navbar" className="navbar navbar-collapse collapse">
                         <ul className="navbar-nav">
@@ -41,94 +41,94 @@ class Header extends Component{
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/managing-directors-message'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Managing Directors's Message
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/mission-vision'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Mission & Vision
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/about-organization'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>About Organization
                                 </a>
 
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/association-scope-portfolio'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Association & Scope & Portfolio
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/ppm-surgicals'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>PPM Surgicals
                                 </a>
                                 <ul>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/products-catalogue">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Product's Catalogue
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/enquiry-form">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Enquiry Form
                                     </li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/ppm-consultancy'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>PPM Consultancy
                                 </a>
                                 <ul>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/consultancy-portfolio">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Consultancy Portfolio
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/get-in-touch-form">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Get-in-Touch Form
                                     </li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/ppm-constructions'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>PPM Constructions
                                 </a>
                                 <ul>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/civil-construction">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Civil Construction
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/architectural-structural">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Architecture & Structural
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/designs">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Designs
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/interior-design">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Interior Designing
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/associate-form">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Associate Form
                                     </li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href='/'>
+                                <a className="nav-link" href='/ppm-event-management'>
                                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>PPM Event Management
                                 </a>
                                 <ul>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/services-profile">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Services Profile
                                     </li>
-                                    <li className="nav-link" href="/">
+                                    <li className="nav-link" href="/call-form">
                                         <span className="glyphicon glyphicon-home" aria-hidden="true"></span>Call Form
                                     </li>
                                 </ul>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link" href='/aboutus'>
+                                <a className="nav-link" href='/general-rules-regulations'>
                                     <span className="glyphicon glyphicon-info-sign" aria-hidden="true"></span> General Rules & Regulations
                                 </a>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link" href='/library'>
+                                <a className="nav-link" href='/achievementsgallery'>
                                     <span className="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Achievement's Gallery
                                 </a>
                             </li>
@@ -140,41 +140,41 @@ class Header extends Component{
                         </ul>
                     </div>
 
-                    <div className="navbar navbar-collapse collapse">
-                        {
-                            (this.state.showComponent) ?
-                                <ul className="navbar-nav ">
+                    {/*<div className="navbar navbar-collapse collapse">*/}
+                        {/*{*/}
+                            {/*(this.state.showComponent) ?*/}
+                                {/*<ul className="navbar-nav ">*/}
 
-                                    <li className="nav-item">
-                                        <span className="nav-link"> Welcome, User </span>
-                                    </li>
+                                    {/*<li className="nav-item">*/}
+                                        {/*<span className="nav-link"> Welcome, User </span>*/}
+                                    {/*</li>*/}
 
-                                    <li className="nav-item">
-                                        <a className="nav-link" href='/logout'>
-                                            <span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Logout
-                                        </a>
-                                    </li>
-                                </ul>
-                                :
-                                <ul className="navbar-nav ">
-                                    <li className="nav-item">
-                                        <a className="nav-link " href='/login'>
-                                            <span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Login
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link ">
-                                        /
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link " href='/register'>
-                                            <span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Register
-                                        </a>
-                                    </li>
-                                </ul>
-                        }
-                    </div>
+                                    {/*<li className="nav-item">*/}
+                                        {/*<a className="nav-link" href='/logout'>*/}
+                                            {/*<span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Logout*/}
+                                        {/*</a>*/}
+                                    {/*</li>*/}
+                                {/*</ul>*/}
+                                {/*:*/}
+                                {/*<ul className="navbar-nav ">*/}
+                                    {/*<li className="nav-item">*/}
+                                        {/*<a className="nav-link " href='/login'>*/}
+                                            {/*<span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Login*/}
+                                        {/*</a>*/}
+                                    {/*</li>*/}
+                                    {/*<li className="nav-item">*/}
+                                        {/*<a className="nav-link ">*/}
+                                        {/*/*/}
+                                        {/*</a>*/}
+                                    {/*</li>*/}
+                                    {/*<li className="nav-item">*/}
+                                        {/*<a className="nav-link " href='/register'>*/}
+                                            {/*<span className="glyphicon glyphicon-log-in" aria-hidden="true"></span> Register*/}
+                                        {/*</a>*/}
+                                    {/*</li>*/}
+                                {/*</ul>*/}
+                        {/*}*/}
+                    {/*</div>*/}
                 </nav>
                 {/*<header className="jumbotron ">*/}
                     {/*<div className="container">*/}
