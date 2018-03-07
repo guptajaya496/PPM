0804760e4c01974f2765d13ada58ba4874bbe3bf
JG <mail2me17@gmail.com>
2018-03-07 13:35:20 -0800

    changes made

--- Components/call-form.jsx
+++ Components/call-form.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class CallForm extends Component{
+class CallFrom extends Component{
     render(){
         return(
             <div className="container">
